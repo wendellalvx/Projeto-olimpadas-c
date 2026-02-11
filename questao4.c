d86b0bdd6c758daa3d35d40e0c04bdfe4e697828
wendellalvx <wendellthiago02166@gmail.com>
2026-02-10 21:41:20 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -6,8 +6,28 @@ char edicaoAtual[100] = "";
 int totalBrasil = 0;
 fgets(linha, sizeof(linha), arq ); 
 printf(" Evolução das participações do Brasil na Olímpiadas\n");
-
 while(fgets(linha, sizeof(linha), arq)){
 char *linha_copy = strdup(linha);
+char *edicao = strtok(linha_copy, ",");
+char *noc- NULL;
+for(int i=0; i<4;; i++) noc = strtok(NULL, ",");
+if (edicao && noc){
+if(strcmp(edicaoAtual, "") "=0 && strcmp(edicaoAtual, edicao) != 0){
+  if(totalBrasil>0 {
+  printf(%-30s | %d\n", edicaoAtual, totalBrasil); /* basicamente testa se mudou a edição, se mudou já imprime o resultado*/ 
+}
+totalBrasil = 0; /* zera tudo de novo*/
+}
+strcpy(edicaoAtual, edicao);
+if (strcmp(noc, "BRA") ==0){ /* parte mais importante no codigo, porque é aqui que calcula quantas vezes o brasil participou*/
+totalBrasil++;
+}}
+free(linha_copy);}
+if (totalBrasil>0){ /* imprime a ultima linha, porque como no "if" anterior não resolveria porque naõa mudaria a edição, essa parte teve que ser feita*/
+  printf("%-30s | %d\n", edicaoAtual, totalBrasil);
+fclose(arq);}
+
+
+
 
 
